f62b3399e35b6d0e4e60bc44001eb9e22ff93487
MauSant <maubr93@gmail.com>
2021-11-10 20:53:15 -0300

    second commit

---
+++ servidor/server.py
@@ -0,0 +1,10 @@
+
+def function():
+    ''' Função pode ser escrita aqui por cima''' 
+    print('chamando função')
+
+if __name__ == '__main':
+    print('codar aqui')
+    print('se n identar o codigo, n funciona')
+    print('lembra de identar o codigo')
+    function
---
+++ sockets/socket.py
@@ -0,0 +1,15 @@
+
+
+
+class Socket:
+
+    def __init__(self): # <---- Tem que passar o self
+        '''Construtor '''
+        pass
+
+    def method(self):# <---- Tem que passar o self
+        print('methodos')
+        pass
+
+    def method(self):# <---- Tem que passar o self
+        print('Não precisa de get e nem set!!')
