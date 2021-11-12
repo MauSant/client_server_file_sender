77f6e6f4abe7914f71159a993a0e3ead283de804
MauSant <maubr93@gmail.com>
2021-11-12 16:04:29 -0300

    feat: adiciona client_socket (abstração) e refatora algumas funções

--- servidor/server.py
+++ servidor/server.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict
+from typing import Collection, Optional, Dict
 import json
 # from socket import Socket
 from server_config import serverConfig  as config
@@ -16,49 +16,57 @@ def prepare_socket(host:str, port:str):
     return 0
     return socket    
     
-
 def server_listening(socket):
     funcs_dict = load_funcs()
 
     while(True):
-        #Caso a conexão n for aceita, não precisa ler o restante do código e pula pro proximo
-        if not accept_connection(socket):
-            continue
+        permission, client_socket, address = accept_connection(socket)
+        if not permission:
+            continue #Caso a conexão n for aceita, não precisa ler o restante do código e pula pro proximo
 
-        header = receive_from_connection(socket)
 
-        args = load_args(socket,header)
+        header = receive_header(client_socket)
+
+        args = load_args(socket, client_socket ,header)
 
         response = execute_action(
                                   action=header['action'],
                                   funcs_dict=funcs_dict,
                                   args=args
                                  )
+
 '''As funções a serem utilizadas no execute_action, estão aqui '''
 def load_funcs():
     funcs_dict = {
-        "retrieve": retrieve,
-        "send_file": send_file,
         "change_replic": change_replic,
         "store": store,
         "search": search
     } 
     return funcs_dict  
 
-def accept_connection(socket:object):
-    # ...
-    return socket
+def accept_connection(socket:object)->bool:
+    #Retorna True or False E
+    #ACEITA A CONEXÃO e retorna o socket do client
+    # client_socket, address = socket.accept()
+    
+    #para teste
+    permission = True
+    client_socket = 0
+    address = 0
+    return permission, client_socket, address
 
-def receive_from_connection(socket:object) -> Dict:
+def receive_header(socket:object) -> Dict:
     '''Pseudocodigo '''
     #header_json = socket.rcv.decode
     #header = json.loads(header_json)
-    header = 0
+    header = 0 #para testar
     return header
 
-def load_args(socket:object, header:Dict) -> Dict:
+def load_args(socket:object, client_socket:object, header:Dict) -> Dict:
     args = header
     args['socket'] = socket
+    args['client_socket'] = client_socket
+    return args
 
 def execute_action(action:str, funcs_dict: Dict, args: Dict) -> Dict:
     func = funcs_dict[action]
@@ -70,31 +78,21 @@ def change_replic(args):
     print('_change_replic')
     pass
 
-def retrieve(args:Dict) -> bytes:
-    print('retrieve')
-    socket = args['socket']
-    action = args['action']
-    keyword = args['keyword']
-    
-
-    '''Não esquece de colocar o listening'''
-    pass
-
-def send_file(args: Dict) -> None:
+def store(args:Dict) -> str:
     print('send_file')
     socket = args['socket']
     action = args['action']
     file_path = args['file_path']
     replic_number = args['replic_number']
+
     pass
 
-def store(args:Dict):
+def search(args: Dict) -> bytes:
+    print('retrieve')
+    socket = args['socket']
+    action = args['action']
+    keyword = args['keyword']
     pass
-
-def search(args:Dict):
-    pass
-
-
 
 
 
