7696b6383eb529c55fb9d52bfe6fb87e16fd140b
Arriverdecci <yuri.cardosovasconcelos@gmail.comm>
2021-11-20 08:03:49 -0300

    interface completa?

--- cliente/interface/face.py
+++ cliente/interface/face.py
@@ -1,24 +1,28 @@
 def interface():
-    global action
-    print('Bem Vindo ao YBM Storage\n Qual Operação Deseja Realizar ?\n 1-Salvar Arquivo\n 2-Buscar Arquivo\n 3-Alterar numero de Replicas')
-    action = input()
+    
+    while True :
+        global action
+        print('Bem Vindo ao YBM Storage\n Qual Operação Deseja Realizar ?\n 1-Salvar Arquivo\n 2-Buscar Arquivo\n 3-Alterar numero de Replicas\n 4-Sair')
+        action = input()
 
-    if action == '1' :
-        action = 'send'
-        print('Qual o path para o arquvio ?')
-        global local_arquivo
-        local_arquivo = input()
-        print('Quantas replicas desejam salvar ?')
-        global replics
-        replics = input()
-    elif action == '2' :
-        action = 'retrieve'
-        print('Qual o nome do arquivo a ser buscado ?')
-        global nome_arquivo
-        nome_arquivo = input()
-    elif action == '3' :
-        action = 'change'
-        print('Qual o novo numero de replicas a serem salvas ?')
-        replics = input()
+        if action == '1' :
+            action = 'send'
+            print('Qual o path para o arquvio ?')
+            global local_arquivo
+            local_arquivo = input()
+            print('Quantas replicas desejam salvar ?')
+            global replics
+            replics = input()
+        elif action == '2' :
+            ction = 'retrieve'
+            print('Qual o nome do arquivo a ser buscado ?')
+            global nome_arquivo
+            nome_arquivo = input()
+        elif action == '3' :
+            action = 'change'
+            print('Qual o novo numero de replicas a serem salvas ?')
+            replics = input()
+        elif action == '4' :
+            break
 
 interface()
